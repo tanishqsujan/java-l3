0bbd485aed9b211b71cd186e9885f2d3d1b8b99d
tanishqsujan <tanishq.kothamasu@gmail.com>
2024-10-13 11:00:14 +0400

    ten

---
+++ ten.java
@@ -0,0 +1,12 @@
+class ten{
+    public static void main(String[] args){
+        for(int i=10; i>1; i--){
+            System.out.println("The value of i is " +i);
+        }
+        int j=0;
+        while(j<11){
+            System.out.println("The value of j is " +j);
+            j++;
+        }
+    }
+}
