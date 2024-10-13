26de45fae400660b737704fb53d55693379ac9c3
tanishqsujan <tanishq.kothamasu@gmail.com>
2024-10-13 11:07:23 +0400

    hello

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class hello {
+    public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    String name= sc.nextLine();
+    System.out.println("Enter your lucky number: ");
+    int number= sc.nextInt();
+    System.out.println("Hello " +name+ "your lucky number " +number+ "shows how hardworking you are.Keep it up!");
+    }
+}
